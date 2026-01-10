c072e31cb6ddbea3963e5c898edc29ad5b7b0204
Felix Sehnert <felixsehnert@yahoo.de>
2026-01-10 16:30:29 +0100

    - join functionality

--- table.go
+++ table.go
@@ -1,16 +1,35 @@
 package blackjack
 
-import "github.com/Hydoc/deck"
+import (
+	"errors"
+
+	"github.com/Hydoc/deck"
+)
+
+var (
+	ErrTableFull = errors.New("table is full")
+)
 
 // Table represents a blackjack table.
-// A maximum of seven players are allowed at the table.
 type Table struct {
 	dealer  *Player
 	players [7]*Player
 	deck    []deck.Card
 }
 
-// New create a pointer to Table with the default configuration like 6 shuffled decks.
+// Join adds a player to the next nil value in the players slice.
+// It returns ErrTableFull when there is no space left.
+func (t *Table) Join(p *Player) error {
+	for i := range t.players {
+		if t.players[i] == nil {
+			t.players[i] = p
+			return nil
+		}
+	}
+	return ErrTableFull
+}
+
+// New create a pointer to Table with the default configuration like 6 shuffled decks and a maximum of 7 players allowed.
 func New() *Table {
 	return &Table{
 		dealer:  NewPlayer(make([]deck.Card, 0), 0, WithName("Dealer")),
--- table_test.go
+++ table_test.go
@@ -11,7 +11,7 @@ func Test_New(t *testing.T) {
 	table := New()
 	wantDealer := NewPlayer(make([]deck.Card, 0), 0, WithName("Dealer"))
 	wantPlayers := [7]*Player{}
-	wantDeckSize := 52 * 6
+	wantDeckSize := 312
 
 	if len(table.deck) != wantDeckSize {
 		t.Errorf("want deck size %d, got %d", wantDeckSize, len(table.deck))
@@ -23,5 +23,71 @@ func Test_New(t *testing.T) {
 
 	if !reflect.DeepEqual(wantPlayers, table.players) {
 		t.Errorf("want players %v, got %v", wantPlayers, table.players)
+	}
+}
+
+func TestTable_Join(t *testing.T) {
+	tests := []struct {
+		name           string
+		playerToJoin   *Player
+		playersAtTable [7]*Player
+		wantIndex      int
+		wantErr        error
+	}{
+		{
+			name:           "with empty slice of players at table",
+			playerToJoin:   NewPlayer(make([]deck.Card, 0), 0, WithName("Player1")),
+			playersAtTable: [7]*Player{},
+			wantIndex:      0,
+			wantErr:        nil,
+		},
+		{
+			name:         "with players at table",
+			playerToJoin: NewPlayer(make([]deck.Card, 0), 0, WithName("Player4")),
+			playersAtTable: [7]*Player{
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player1")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player2")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player3")),
+			},
+			wantIndex: 3,
+			wantErr:   nil,
+		},
+		{
+			name:         "error when table is full",
+			playerToJoin: NewPlayer(make([]deck.Card, 0), 0, WithName("Player8")),
+			playersAtTable: [7]*Player{
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player1")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player2")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player3")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player4")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player5")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player6")),
+				NewPlayer(make([]deck.Card, 0), 0, WithName("Player7")),
+			},
+			wantErr: ErrTableFull,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			table := &Table{
+				players: tt.playersAtTable,
+			}
+
+			err := table.Join(tt.playerToJoin)
+
+			if tt.wantErr != nil {
+				if !reflect.DeepEqual(err, tt.wantErr) {
+					t.Errorf("want error %#v, got %#v", tt.wantErr, err)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("want no error, got %#v", err)
+				}
+				if !reflect.DeepEqual(table.players[tt.wantIndex], tt.playerToJoin) {
+					t.Errorf("want %#v, got %#v", tt.playerToJoin, table.players[tt.wantIndex])
+				}
+			}
+		})
 	}
 }
